2ea3e54a9e0b1447c87bf4a97cb522511f0352ac
Jason <jason.h.7249@gmail.com>
2020-01-19 00:17:41 -0500

    Update App.js

--- frontend/SaferStrides/App.js
+++ frontend/SaferStrides/App.js
@@ -4,58 +4,58 @@ import GetLocationButton from './pages/GetLocationButton';
 import MapView from 'react-native-maps';
 const { width, height } = Dimensions.get('window');
 
-const ASPECT_RATIO = width/height ;
+const ASPECT_RATIO = width / height;
 const LATITUDE = 43.651070;
 const LONGITUDE = -79.347015;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      },
-    };
-  }
-  sendUserLocation = () => {
-    console.log('Pressed button')
-  }
+		this.state = {
+			region: {
+				latitude: LATITUDE,
+				longitude: LONGITUDE,
+				latitudeDelta: LATITUDE_DELTA,
+				longitudeDelta: LONGITUDE_DELTA,
+			},
+		};
+	}
+	sendUserLocation = () => {
+		console.log('Pressed button')
+	}
 
-  render() {
-    return (
-      <View>
-      <MapView
-            provider={this.props.provider}
-            style={styles.map}
-            scrollEnabled={true}
-            zoomEnabled={true}
-            pitchEnabled={true}
-            rotateEnabled={true}
-            initialRegion={this.state.region}
-          >
-          </MapView>   
-        
-        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
+	render() {
+		return (
+			<View>
+				<MapView
+					provider={this.props.provider}
+					style={styles.map}
+					scrollEnabled={true}
+					zoomEnabled={true}
+					pitchEnabled={true}
+					rotateEnabled={true}
+					initialRegion={this.state.region}
+				>
+				</MapView>
+
+				{/* <Text style={styles.welcome}>Welcome to React Native!</Text>
         <GetLocationButton onGetLocation={this.sendUserLocation} />
         <Text style={styles.instructions}>{instructions}</Text> */}
-      </View>
-    );
-  }
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  scrollview: {
-    alignItems: 'center',
-    paddingVertical: 40,
-  },
-  map: {
-    width: width,
-    height: height,
-  },
+	scrollview: {
+		alignItems: 'center',
+		paddingVertical: 40,
+	},
+	map: {
+		width: width,
+		height: height,
+	},
 });
