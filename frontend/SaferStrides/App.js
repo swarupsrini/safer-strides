669548dd0b6314e3ff267197760cec61956ea140
swarupsrini <swarup.srinivasan@mail.utoronto.ca>
2020-01-19 00:28:32 -0500

    fixed app, implemented navigation

--- frontend/SaferStrides/App.js
+++ frontend/SaferStrides/App.js
@@ -1,51 +1,67 @@
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Dimensions } from 'react-native';
-import GetLocationButton from './pages/GetLocationButton';
-import MapView from 'react-native-maps';
-const { width, height } = Dimensions.get('window');
+import Terms from './pages/Terms';
+import MapStreetView from './pages/MapStreetView';
+import MapHeat from './pages/MapHeat';
 
-const ASPECT_RATIO = width/height ;
-const LATITUDE = 43.651070;
-const LONGITUDE = -79.347015;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+// import GetLocationButton from './pages/GetLocationButton';
+// import MapView from 'react-native-maps';
+// const { width, height } = Dimensions.get('window');
+
+// const ASPECT_RATIO = width/height ;
+// const LATITUDE = 43.651070;
+// const LONGITUDE = -79.347015;
+// const LATITUDE_DELTA = 0.0922;
+// const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
 
 export default class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      },
+        step: 1,
+    //   region: {
+    //     latitude: LATITUDE,
+    //     longitude: LONGITUDE,
+    //     latitudeDelta: LATITUDE_DELTA,
+    //     longitudeDelta: LONGITUDE_DELTA,
+    //   },
     };
   }
-  sendUserLocation = () => {
-    console.log('Pressed button')
-  }
+//   sendUserLocation = () => {
+//     console.log('Pressed button')
+//   }
 
   render() {
-    return (
-      <View>
-      <MapView
-            provider={this.props.provider}
-            style={styles.map}
-            scrollEnabled={true}
-            zoomEnabled={true}
-            pitchEnabled={true}
-            rotateEnabled={true}
-            initialRegion={this.state.region}
-          >
-          </MapView>   
+    const { step } = this.state;
+    switch(step) {
+        case 1:
+            return (<Terms />);
+        case 2:
+            return (<MapStreetView />);
+        case 3:
+            return (<MapHeat />);
+    }
+
+    // return (
+    //   <View>
+          
+    //   {/* <MapView
+    //         provider={this.props.provider}
+    //         style={styles.map}
+    //         scrollEnabled={true}
+    //         zoomEnabled={true}
+    //         pitchEnabled={true}
+    //         rotateEnabled={true}
+    //         initialRegion={this.state.region}
+    //       >
+    //       </MapView>    */}
         
-        {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <GetLocationButton onGetLocation={this.sendUserLocation} />
-        <Text style={styles.instructions}>{instructions}</Text> */}
-      </View>
-    );
+    //     {/* <Text style={styles.welcome}>Welcome to React Native!</Text>
+    //     <GetLocationButton onGetLocation={this.sendUserLocation} />
+    //     <Text style={styles.instructions}>{instructions}</Text> */}
+    //   </View>
+    // );
   }
 }
 
